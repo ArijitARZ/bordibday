e94808ff95f547da0995f4833166970e4686c371
ArijitARZ <143308314+ArijitARZ@users.noreply.github.com>
2023-11-01 12:53:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+window.addEventListener('click', function () {
+    
+   
+    
+});
+alert(" Please Increase Your Mobile Volume For Listening Song Bordi.🎵🎧🔈");
+alert("This is for u from me and ❤️didivai❤️")
+alert("Happy Birthday Bordiii🍰🎂")
+
+function next() {
+
+  
+  document.querySelector(".cont").style.display= "none";
+ 
+   document.querySelector(".second_page").style.display = "block";
+
+document.querySelector(".b2").style.display = "none";
+
+
+document.querySelector(".b1").style.display = "block"; document.querySelector("body").style.overflow = "scroll";
+
+ }
+function prev() {
+
+  
+  document.querySelector(".cont").style.display= "block";
+ 
+   document.querySelector(".second_page").style.display = "none";
+   
+document.querySelector(".b2").style.display = "block";
+
+
+document.querySelector(".b1").style.display = "none";
+   document.querySelector("body").style.overflow = "hidden";
+
+}
